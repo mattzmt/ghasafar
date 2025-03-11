7b008c4bfaf1be2f13dcd1a36b74646053e2be37
matt z <mattzhidden@duck.com>
2025-03-11 08:07:51 +0100

    Trying to make grid divs have random bg imgs

--- js/index.js
+++ js/index.js
@@ -10,4 +10,13 @@ function adjustGrid() {
 }
 
 window.addEventListener('resize', adjustGrid);
-window.addEventListener('load', adjustGrid);  
+window.addEventListener('load', adjustGrid);
+
+fetch('js/birds.json')
+    .then(response => response.json())
+    .then(data => {
+        const randomItem = data[Math.floor(Math.random() * data.length)];
+        const randomSmallImg = randomItem.smallImg;
+        document.getElementById('wtd-birds').style.backgroundImage = `url('${randomSmallImg}')`;
+    })
+    .catch(error => console.error('Error loading images:', error));
