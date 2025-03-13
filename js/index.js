ca76199782b6c90d5911dd537a9452990dddbe66
matt z <mattzhidden@duck.com>
2025-03-13 23:45:42 +0100

    Updated places page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-function adjustGrid() {
-    const parent = document.querySelector('#wtd');
-    const containerWidth = parent.clientWidth;
-    const itemMinWidth = 170;
-  
-    let cols = Math.floor(containerWidth / itemMinWidth);
-    if (cols % 2 !== 0) cols--;
-  
-    parent.style.gridTemplateColumns = `repeat(${Math.max(cols, 2)}, 1fr)`;
-}
-
-window.addEventListener('resize', adjustGrid);
-window.addEventListener('load', adjustGrid);
-
 fetch('js/birds.json')
     .then(response => response.json())
     .then(data => {
