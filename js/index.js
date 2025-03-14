95fe9e7b7e72a6d49fc65262bc55933bc6d39a07
matt z <mattzhidden@duck.com>
2025-03-14 11:47:56 +0100

    Made index.js actually load places imgs not birds

--- js/index.js
+++ js/index.js
@@ -4,9 +4,14 @@ fetch('js/birds.json')
         const randomBird = data[Math.floor(Math.random() * data.length)];
         const randomSmallBirdImg = randomBird.smallImg;
         document.getElementById('wtd-birds').style.backgroundImage = `url('${randomSmallBirdImg}')`;
+    })
+    .catch(error => console.error('Error loading birds.json:', error));
 
+fetch('js/places.json')
+    .then(response => response.json())
+    .then(data => {
         const randomPlace = data[Math.floor(Math.random() * data.length)];
         const randomSmallPlaceImg = randomPlace.smallImg;
         document.getElementById('wtd-places').style.backgroundImage = `url('${randomSmallPlaceImg}')`;
     })
-    .catch(error => console.error('Error loading images:', error));
+    .catch(error => console.error('Error loading places.json:', error));
