6cd40086ad5413417ee097e33ca91452458bd7ea
matt z <mattzhidden@duck.com>
2025-03-14 23:42:03 +0100

    Updated various things

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ fetch('js/birds.json')
     .then(response => response.json())
     .then(data => {
         const randomBird = data[Math.floor(Math.random() * data.length)];
-        const randomSmallBirdImg = randomBird.smallImg;
-        document.getElementById('wtd-birds').style.backgroundImage = `url('${randomSmallBirdImg}')`;
+        const randomBirdImg = randomBird.img;
+        document.getElementById('wtd-birds').style.backgroundImage = `url('${randomBirdImg}')`;
     })
     .catch(error => console.error('Error loading birds.json:', error));
 
@@ -11,7 +11,7 @@ fetch('js/places.json')
     .then(response => response.json())
     .then(data => {
         const randomPlace = data[Math.floor(Math.random() * data.length)];
-        const randomSmallPlaceImg = randomPlace.smallImg;
-        document.getElementById('wtd-places').style.backgroundImage = `url('${randomSmallPlaceImg}')`;
+        const randomPlaceImg = randomPlace.img;
+        document.getElementById('wtd-places').style.backgroundImage = `url('${randomPlaceImg}')`;
     })
     .catch(error => console.error('Error loading places.json:', error));
